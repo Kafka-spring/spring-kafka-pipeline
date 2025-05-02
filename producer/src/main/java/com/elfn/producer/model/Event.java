a9ae24f97893b961eecae7b7a4ac0bfdfa7141a4
Elfn <elimanefofana17@gmail.com>
2025-05-02 14:57:59 +0200

    Evénement métier à envoyer vers Kafka.

---
+++ producer/src/main/java/com/elfn/producer/model/Event.java
@@ -0,0 +1,23 @@
+package com.elfn.producer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.Instant;
+
+/**
+ * @Author: Elimane
+ *
+ * Représente un événement métier à envoyer vers Kafka.
+ */
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    private String id;
+    private Instant timestamp;
+}
